9d77c1ea2e3ef975c82e5883cbc3ea9beecfd958
CheaVuThearith <cheavuthearith0@gmail.com>
2024-04-05 23:54:04 +0700

    card one row only

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import Card from '../_components/Card'
+import React from "react";
+import Card from "../_components/Card";
 
-type Props = {}
+type Props = {};
 
 const page = async (props: Props) => {
-  return (<>
-  <div className='flex overflow-hidden gap-y-2 flex-wrap gap-x-6'>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  <Card type='big'/>
-  </div>
+  return (
+    <>
+      <div
+        className="grid max-h-56 auto-rows-min gap-x-6 gap-y-2 overflow-hidden"
+        style={{ gridTemplateColumns: "repeat(auto-fill, minmax(160px, 1fr))" }}
+      >
+        {
+          //TODO:FIX THIS
+        }
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+        <Card type="big" />
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default page
+export default page;
